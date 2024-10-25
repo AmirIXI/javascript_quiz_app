3f162d839bb11a845d66b503dbb87f2911ce77e4
Amir <amir@email.com>
2024-10-25 16:23:18 +0330

    initial files and quiz list

---
+++ math-quiz.js
@@ -0,0 +1,32 @@
+const quiz_list = [
+    {
+        q_num: 1,
+        q_body: "2 + 2",
+        q_ansewr: 4,
+        q_point: 1,
+    },
+    {
+        q_num: 2,
+        q_body: "6 x 4",
+        q_ansewr: 24,
+        q_point: 3,
+    },
+    {
+        q_num: 3,
+        q_body: "3 x 3 x 9",
+        q_ansewr: 81,
+        q_point: 8,
+    },
+    {
+        q_num: 4,
+        q_body: "21 + 30 - 1",
+        q_ansewr: 50,
+        q_point: 5,
+    },
+    {
+        q_num: 5,
+        q_body: "36 % 6",
+        q_ansewr: 6,
+        q_point: 3,
+    },
+]
